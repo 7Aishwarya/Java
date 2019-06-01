08ba08676d7f46730545e26466d7fcdc18656dd4
Aishwarya Sharma <33029482+7Aishwarya@users.noreply.github.com>
2019-06-01 17:37:08 +0530

    constructor overloading

---
+++ More in java/FiveObjects.java
@@ -0,0 +1,47 @@
+//constructor overloading
+
+class FiveObjects
+
+{
+
+int a;
+
+FiveObjects()
+
+{
+
+a=5;
+
+System.out.println("Default constructor called. Value of a = "+a);
+
+}
+
+FiveObjects(int x)
+
+{
+
+a=x;
+
+System.out.println("Parameterized constructor called. Value of a = "+a);
+
+}
+
+
+
+public static void main(String args[])
+
+{
+
+FiveObjects obj1 = new FiveObjects();
+
+FiveObjects obj2 = new FiveObjects(1);
+
+FiveObjects obj3 = new FiveObjects(2);
+
+FiveObjects obj4 = new FiveObjects(3);
+
+FiveObjects obj5 = new FiveObjects(4);
+
+}
+
+}
