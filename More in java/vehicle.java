bf97f59043d5e815016799810a8a5c5b17f43cf0
Aishwarya Sharma <33029482+7Aishwarya@users.noreply.github.com>
2019-06-01 17:39:33 +0530

    method overloading

---
+++ More in java/vehicle.java
@@ -0,0 +1,42 @@
+//method overloading
+
+class vehicle
+
+{
+
+void car()
+
+{
+
+System.out.println("car by default");
+
+}
+
+void car(int x)
+
+{
+
+System.out.println("car 1");
+}
+void car(int x,int y)
+
+{
+
+System.out.println("car 2");
+}
+
+
+
+public static void main(String args[])
+
+{
+
+vehicle v = new vehicle();
+
+v.car();
+
+v.car(1);
+v.car(1,2);
+}
+
+}
