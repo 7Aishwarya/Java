0cafd1f28134dfdfdc22c20b7d220dbd8a9d71a4
Aishwarya Sharma <33029482+7Aishwarya@users.noreply.github.com>
2019-06-13 10:42:06 +0530

    Threads in java

---
+++ More in java/currentthreaddemo.java
@@ -0,0 +1,41 @@
+class myclass extends Thread
+
+{
+
+public void run()
+
+{
+
+for(int i=0;i<10;i++)
+
+{
+
+System.out.println(Thread.currentThread().getId() + "value" + i);
+
+}
+
+}
+
+}
+
+public class currentthreaddemo
+
+{
+
+public static void main(String args[])
+
+{
+
+myclass myclass= new myclass();
+
+myclass.start();
+
+myclass myclass1=new myclass();
+
+myclass1.start();
+
+}
+
+}
+
+//System.out.println(thread.currentthread().getID()+"value"+i);
